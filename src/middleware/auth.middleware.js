94364de2f9e37d4fdd3e567c78af7f5de10c430a
Gobo13 <Gobo13@hotmail.com>
2025-04-03 21:16:59 +0200

    Signed-off-by: Gobo13 <Gobo13@hotmail.com>

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,9 +1,9 @@
 function requireAuth(req, res, next) {
 	if (req.session) {// && req.session.username) {
-        //res.locals.authenticated = true;
+        res.locals.authenticated = true;
 		next(); // User is authenticated, continue to the requested page
     } else {
-        //res.locals.authenticated = false;
+        res.locals.authenticated = false;
 		res.redirect('/auth/login'); // Redirect to login if not authenticated
     }
 }
