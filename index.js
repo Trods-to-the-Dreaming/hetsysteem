b36cdc4d53f9483e4c5c70b82ddcfa5db269b77a
Gobo13 <Gobo13@hotmail.com>
2025-04-10 08:29:39 +0200

    Signed-off-by: Gobo13 <Gobo13@hotmail.com>

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: false, //process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV === "production", // true --> werkt niet // false, // --> werkt wel, maar onveilig?
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000
   }
