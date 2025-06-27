c816aa2b99ddcd928833d83d28214768afa36fc5
Gobo13 <Gobo13@hotmail.com>
2025-06-27 19:14:21 +0200

    Signed-off-by: Gobo13 <Gobo13@hotmail.com>

--- src/controllers/cron.controllers.js
+++ src/controllers/cron.controllers.js
@@ -2,14 +2,19 @@ import db from "../utils/db.js";
 
 export const processOrders = async (req, res, next) => {
 	try {
+		const logFile = path.join(process.cwd(), "logs", "cron.log");
+		fs.appendFileSync(logFile, `${new Date().toISOString()} - processOrders gestart\n`);
+	
 		const authHeader = req.headers.authorization;
 		const token = authHeader?.split(" ")[1];
+		fs.appendFileSync(logFile, `Token ontvangen: ${token}\n`);
 
 		if (token !== process.env.CRON_TOKEN) {
+			fs.appendFileSync(logFile, `Toegang geweigerd\n`);
 			return res.status(403).send("Toegang geweigerd");
 		}
 		
-		const newQuantity = 9;
+		const newQuantity = 321;
 		const characterId = 1;
 		const productId = 4;
 	  
@@ -23,8 +28,11 @@ export const processOrders = async (req, res, next) => {
 			 productId]
 		);
 		
+		fs.appendFileSync(logFile, `Query uitgevoerd\n`);
 		res.status(200).send("Cron job succesvol uitgevoerd");
 	} catch (err) {
+		const logFile = path.join(process.cwd(), "logs", "cron.log");
+		fs.appendFileSync(logFile, `Fout: ${err.message}\n`);
 		next(err); 
 	}
 }
